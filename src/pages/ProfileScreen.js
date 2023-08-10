c38a6d5e9de923bb4ec1dd85197245842667ce78
javamaster94 <emindutt@hotmail.com>
2023-08-10 03:18:51 +0300

    LoginScreen Edited

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -9,14 +9,42 @@ function ProfileScreen() {
     const navigation = useNavigation();
 
     return (
-      <View style={{ flex:1, alignItems: 'center', justifyContent: 'center',backgroundColor:"cyan"}}>
+      <View style={{ flex: 1, backgroundColor: 'lightgray' }}>
+  <View style={{ height: 300, backgroundColor: 'skyblue'}}>
+  
+  </View>
+  <View style={{ flex: 1, paddingHorizontal:10, backgroundColor:"white",}}>
+    <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20,fontWeight: 900 }}>
+      Emin_Ex  
+    </Text>
+    <Text style={{ fontSize: 16, color: 'gray', marginBottom: 10, fontWeight: 900 }}>
+      Beğeni: 500
+    </Text>
+   
+    <View style={{ flex: 0.3, justifyContent: 'center', marginBottom: 2, borderWidth: 0.5, borderColor: 'gray',  borderRadius: 5,backgroundColor:"white"}}>
+      
+      <Text style={{ fontSize: 12, color: 'gray',marginLeft:20,fontWeight: 900 }}>Takipçi</Text>
+    </View>
+  
+    <View style={{ flex: 0.3, justifyContent: 'center', marginBottom: 2, borderWidth: 0.5, borderColor: 'gray',  borderRadius: 5,backgroundColor:"white"}}>
+      
+      <Text style={{ fontSize: 12, color: 'gray' ,marginLeft:20,fontWeight: 900  }}>Takip Edilen</Text>
+    </View>
+   
+    <View style={{ flex: 0.3, justifyContent: 'center', marginBottom: 2, borderWidth: 0.5, borderColor: 'gray', borderRadius: 5,backgroundColor:"white"}}>
+      
+      <Text style={{ fontSize: 12, color: 'gray' ,marginLeft:20,fontWeight: 900 }}>Mesaj</Text>
+    </View>
+    <View style={{ flex: 0.3, justifyContent: 'center', marginBottom: 2, borderWidth: 0.5 , borderColor: 'gray',  borderRadius: 5,backgroundColor:"white"}}>
+      
+      <Text style={{ fontSize: 12, color: 'gray' ,marginLeft:20,fontWeight: 900 }}>sada</Text>
+    </View>
+  </View>
+</View>
 
-        <Text>ProfileScreen</Text>
-        <Ionicons name="hand-right" color={"blue"} size={250} />
-        
 
+     
 
-      </View>
     );
   }
 
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import LoginScreen from './LoginScreen';
@@ -10,44 +10,76 @@ import SearchScreen from './SearchScreen';
 import ProfileScreen from './ProfileScreen';
 import SettingsScreen from './SettingsScreen';
 
-
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-
-
 
 const Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='login' screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="login"
+        screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="HomeScreen">
           {() => (
-            <Tab.Navigator screenOptions={{headerShown:false}}>
-              <Tab.Screen name="Home" options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={"tomato"} size={size} />
-          ),
-        }} component={HomeScreen} />
-              <Tab.Screen name="Search" options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="magnify" color={"tomato"} size={size} />
-          ),
-        }}  component={SearchScreen} />
-              <Tab.Screen name="Profile" options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account-circle" color={"tomato"} size={size} />
-          ),
-        }} component={ProfileScreen} />
-        <Tab.Screen name="Settings" options={{
-          tabBarLabel: 'Settings',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="cog" color={"tomato"} size={size} />
-          ),
-        }} component={SettingsScreen} />
+            <Tab.Navigator screenOptions={{headerShown: false}}>
+              <Tab.Screen
+                name="Home"
+                options={{
+                  //İCONLARIN İSMİNİ BURADAN DEĞİŞTİR.
+                  tabBarLabel: 'Home',
+                  tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons
+                      name="home"
+                      color={'tomato'}
+                      size={size}
+                    />
+                  ),
+                }}
+                component={HomeScreen}
+              />
+              <Tab.Screen
+                name="Search"
+                options={{
+                  tabBarLabel: 'Search',
+                  tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons
+                      name="magnify"
+                      color={'tomato'}
+                      size={size}
+                    />
+                  ),
+                }}
+                component={SearchScreen}
+              />
+              <Tab.Screen
+                name="Profile"
+                options={{
+                  tabBarLabel: 'Profil',
+                  tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons
+                      name="account-circle"
+                      color={'tomato'}
+                      size={size}
+                    />
+                  ),
+                }}
+                component={ProfileScreen}
+              />
+              <Tab.Screen
+                name="Settings"
+                options={{
+                  tabBarLabel: 'Settings',
+                  tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons
+                      name="cogs"
+                      color={'tomato'}
+                      size={size}
+                    />
+                  ),
+                }}
+                component={SettingsScreen}
+              />
             </Tab.Navigator>
           )}
         </Stack.Screen>
